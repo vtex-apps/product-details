ee30c6cb2929ed5788ea6cd7b866f77eba41a3cf
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-08 16:41:03 -0300

    Add first files to link the app

---
+++ react/ProductDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import { compose } from 'react-apollo'
+
+// TODO: Implement Component logic
+class ProductDetails extends Component {
+  render() {
+    return <div className="tc">It Works!</div>
+  }
+}
+
+export default compose()(ProductDetails)
