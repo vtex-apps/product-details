871a62728a624da16e93282c987bdabc768b5d29
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-09 15:23:38 -0300

    Add the product query and remove the product mocks

---
+++ react/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProductDetails from './ProductDetails'
+
+// TODO: Delete this file when the Component is finished.
+const STORECOMPONENTS_SLUG = 'porsche-911'
+
+export default function index() {
+  return <ProductDetails slug={STORECOMPONENTS_SLUG} />
+}
