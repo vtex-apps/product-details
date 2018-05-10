99d0f06eac32ccf4b2cbcf78765a1651e5dd795f
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-10 15:51:04 -0300

    Add the linked BuyButton and begining the mobile classes of ProductDetails

--- react/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ProductDetails from './ProductDetails'
-
-// TODO: Delete this file when the Component is finished.
-const STORECOMPONENTS_SLUG = 'motorola-moto-x4'
-
-export default function index() {
-  return <ProductDetails slug={STORECOMPONENTS_SLUG} />
-}
