0dc11154a992ebb914c252750cb715e8c380cb98
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-28 16:56:59 -0300

    Refact PropTypes

--- react/ProductDetails.js
+++ react/ProductDetails.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
+import { FormattedMessage, injectIntl } from 'react-intl'
 import { graphql } from 'react-apollo'
 
 import BuyButton from 'vtex.store-components/BuyButton'
@@ -12,6 +11,7 @@ import ShippingSimulator from 'vtex.store-components/ShippingSimulator'
 
 import Spinner from '@vtex/styleguide/lib/Spinner'
 
+import ProductDetailsPropTypes from './propTypes'
 import productQuery from './graphql/productQuery.gql'
 
 import './global.css'
@@ -93,76 +93,7 @@ class ProductDetails extends Component {
     )
   }
 
-  static propTypes = {
-    /** Product slug */
-    slug: PropTypes.string.isRequired,
-    /** Product that owns the informations */
-    data: PropTypes.shape({
-      product: PropTypes.shape({
-        /** Product's id */
-        productId: PropTypes.string.isRequired,
-        /** Product's name */
-        productName: PropTypes.string.isRequired,
-        /** Product's brand */
-        brand: PropTypes.string.isRequired,
-        /** Product's SKUs */
-        items: PropTypes.arrayOf(
-          PropTypes.shape({
-            /** SKU id */
-            itemId: PropTypes.string.isRequired,
-            /** SKU name */
-            name: PropTypes.string.isRequired,
-            /** SKU Images to be shown */
-            images: PropTypes.arrayOf(
-              PropTypes.shape({
-                /** Image id */
-                imageId: PropTypes.string.isRequired,
-                /** Image label */
-                imageLabel: PropTypes.string,
-                /** Image tag as string */
-                imageTag: PropTypes.string,
-                /** Image URL */
-                imageUrl: PropTypes.string.isRequired,
-                /** Image text */
-                imageText: PropTypes.string.isRequired,
-              })
-            ).isRequired,
-            /** SKU sellers */
-            sellers: PropTypes.arrayOf(
-              PropTypes.shape({
-                /** Seller id */
-                sellerId: PropTypes.string.isRequired,
-                /** Seller comertial offer */
-                commertialOffer: PropTypes.shape({
-                  /** SKU installments */
-                  Installments: PropTypes.arrayOf(
-                    PropTypes.shape({
-                      /** Installment value */
-                      Value: PropTypes.number.isRequired,
-                      /** Interest rate (zero if interest-free) */
-                      InterestRate: PropTypes.number.isRequired,
-                      /** Calculated total value */
-                      TotalValuePlusInterestRate: PropTypes.number,
-                      /** Number of installments */
-                      NumberOfInstallments: PropTypes.number.isRequired,
-                      /** Installments offer name */
-                      Name: PropTypes.string,
-                    })
-                  ),
-                  /** Selling Price */
-                  Price: PropTypes.number.isRequired,
-                  /** List Price */
-                  ListPrice: PropTypes.number.isRequired,
-                }).isRequired,
-              })
-            ).isRequired,
-          })
-        ).isRequired,
-      }),
-    }),
-    /** intl property to format data */
-    intl: intlShape.isRequired,
-  }
+  static propTypes = ProductDetailsPropTypes
 }
 
 const options = {
