517442cff35244502cffb708158277dc55bd8ad2
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-09-14 15:19:33 -0300

    Add breadcrumb extension point

--- react/ProductDetails.js
+++ react/ProductDetails.js
@@ -1,7 +1,5 @@
-import './global.css'
-
-import { mapObjIndexed, mergeDeepRight, path } from 'ramda'
 import React, { Component, Fragment } from 'react'
+import { mapObjIndexed, mergeDeepRight, path } from 'ramda'
 import { FormattedMessage } from 'react-intl'
 import {
   AvailabilitySubscriber,
@@ -14,9 +12,12 @@ import {
   ShippingSimulator,
   SKUSelector,
 } from 'vtex.store-components'
+import { ExtensionPoint } from 'render'
 
 import IntlInjector from './components/IntlInjector'
 import ProductDetailsPropTypes from './propTypes'
+
+import './global.css'
 
 const { account } = global.__RUNTIME__
 const productNameLoaderStyles = {
@@ -32,6 +33,7 @@ const productNameLoaderStyles = {
     height: '1.045em',
   },
 }
+
 const productPriceLoaderStyles = {
   'vtex-price-list__container--loader': {
     x: 0,
@@ -138,7 +140,7 @@ class ProductDetails extends Component {
   get selectedItem() {
     const items = path(['productQuery', 'product', 'items'], this.props) || []
     if (!this.props.query.skuId) return items[0]
-    const [selected] = items.filter(sku => sku.itemId == this.props.query.skuId)
+    const [selected] = items.filter(sku => sku.itemId === this.props.query.skuId)
     return selected
   }
 
@@ -154,6 +156,9 @@ class ProductDetails extends Component {
     const {
       productQuery: { product },
       displayVertically,
+      term,
+      slug,
+      categories,
     } = this.props
 
     const showBuyButton =
@@ -163,125 +168,134 @@ class ProductDetails extends Component {
     return (
       <IntlInjector>
         {intl => (
-          <div className="vtex-product-details vtex-page-padding flex flex-wrap pa6">
-            <div className="vtex-product-details__images-container w-50-ns w-100-s pr5-ns">
-              <div className="fr-ns w-100 h-100">
-                <div className="flex justify-center pt2">
-                  <ProductImages
-                    images={path(['images'], this.selectedItem)}
-                    thumbnailSliderOrientation={
-                      displayVertically ? 'VERTICAL' : 'HORIZONTAL'
-                    }
-                  />
+          <div className="vtex-page-padding">
+            <ExtensionPoint
+              id="breadcrumb"
+              term={term}
+              slug={slug}
+              categories={categories}
+            />
+
+            <div className="vtex-product-details flex flex-wrap">
+              <div className="vtex-product-details__images-container w-50-ns w-100-s pr5-ns">
+                <div className="fr-ns w-100 h-100">
+                  <div className="flex justify-center pt2">
+                    <ProductImages
+                      images={path(['images'], this.selectedItem)}
+                      thumbnailSliderOrientation={
+                        displayVertically ? 'VERTICAL' : 'HORIZONTAL'
+                      }
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="vtex-product-details__details-container w-50-ns w-100-s pl5-ns">
-              <div className="fl-ns w-100">
-                <div className="vtex-product-details__name-container pv2">
-                  <ProductName
-                    styles={productNameLoaderStyles}
-                    name={path(['productName'], product)}
-                    skuName={path(['name'], this.selectedItem)}
-                    brandName={path(['brand'], product)}
-                    productReference={path(['productReference'], product)}
-                    {...this.props.name}
-                  />
-                </div>
-                {(Number.isNaN(
-                  +path(['AvailableQuantity'], this.commertialOffer)
-                ) ||
-                  path(['AvailableQuantity'], this.commertialOffer) > 0) && (
-                  <div className="vtex-product-details__price-container pt1">
-                    <ProductPrice
-                      styles={productPriceLoaderStyles}
-                      listPrice={path(['ListPrice'], this.commertialOffer)}
-                      sellingPrice={path(['Price'], this.commertialOffer)}
-                      installments={path(
-                        ['Installments'],
-                        this.commertialOffer
-                      )}
-                      {...this.props.price}
+              <div className="vtex-product-details__details-container w-50-ns w-100-s pl5-ns">
+                <div className="fl-ns w-100">
+                  <div className="vtex-product-details__name-container pv2">
+                    <ProductName
+                      styles={productNameLoaderStyles}
+                      name={path(['productName'], product)}
+                      skuName={path(['name'], this.selectedItem)}
+                      brandName={path(['brand'], product)}
+                      productReference={path(['productReference'], product)}
+                      {...this.props.name}
                     />
                   </div>
-                )}
-                <div className="pv2">
-                  <hr className="o-30" size="1" />
-                </div>
-                <div>
-                  <div className="f7">
-                    <FormattedMessage id="sku-label" />
-                  </div>
-                  {product && (
-                    <SKUSelector
-                      skuItems={this.skuItems}
-                      skuSelected={this.selectedItem}
-                      productSlug={product.linkText}
-                    />
-                  )}
-                </div>
-                <div className="pv2">
-                  <hr className="o-30" size="1" />
-                </div>
-                {showBuyButton ? (
-                  <Fragment>
-                    <div className="pv2">
-                      <BuyButton
-                        skuItems={
-                          this.selectedItem &&
-                          this.sellerId && [
-                            {
-                              skuId: this.selectedItem.itemId,
-                              quantity: 1,
-                              seller: this.sellerId,
-                            },
-                          ]
-                        }>
-                        <FormattedMessage id="button-label" />
-                      </BuyButton>
-                    </div>
-                    <div className="pv4">
-                      {/* FIXME: Get this country data correctly */}
-                      <ShippingSimulator
-                        skuId={path(['itemId'], this.selectedItem)}
-                        seller={this.sellerId}
-                        country="BRA"
+                  {(Number.isNaN(
+                    +path(['AvailableQuantity'], this.commertialOffer)
+                  ) ||
+                    path(['AvailableQuantity'], this.commertialOffer) > 0) && (
+                    <div className="vtex-product-details__price-container pt1">
+                      <ProductPrice
+                        styles={productPriceLoaderStyles}
+                        listPrice={path(['ListPrice'], this.commertialOffer)}
+                        sellingPrice={path(['Price'], this.commertialOffer)}
+                        installments={path(
+                          ['Installments'],
+                          this.commertialOffer
+                        )}
+                        {...this.props.price}
                       />
                     </div>
-                  </Fragment>
-                ) : (
-                  <div className="pv4">
-                    <AvailabilitySubscriber skuId={this.selectedItem.itemId} />
+                  )}
+                  <div className="pv2">
+                    <hr className="o-30" size="1" />
                   </div>
-                )}
-                <div className="flex w-100 pv2">
-                  <div className="pv2 pr3 f6">
-                    <FormattedMessage id="share.label" />:
-                  </div>
-                  <Share
-                    {...this.props.share}
-                    loading={!path(['name'], this.selectedItem)}
-                    title={intl.formatMessage(
-                      { id: 'share.title' },
-                      {
-                        product: path(['productName'], product),
-                        sku: path(['name'], this.selectedItem),
-                        store: account,
-                      }
+                  <div>
+                    <div className="f7">
+                      <FormattedMessage id="sku-label" />
+                    </div>
+                    {product && (
+                      <SKUSelector
+                        skuItems={this.skuItems}
+                        skuSelected={this.selectedItem}
+                        productSlug={product.linkText}
+                      />
                     )}
-                  />
+                  </div>
+                  <div className="pv2">
+                    <hr className="o-30" size="1" />
+                  </div>
+                  {showBuyButton ? (
+                    <Fragment>
+                      <div className="pv2">
+                        <BuyButton
+                          skuItems={
+                            this.selectedItem &&
+                            this.sellerId && [
+                              {
+                                skuId: this.selectedItem.itemId,
+                                quantity: 1,
+                                seller: this.sellerId,
+                              },
+                            ]
+                          }>
+                          <FormattedMessage id="button-label" />
+                        </BuyButton>
+                      </div>
+                      <div className="pv4">
+                        {/* FIXME: Get this country data correctly */}
+                        <ShippingSimulator
+                          skuId={path(['itemId'], this.selectedItem)}
+                          seller={this.sellerId}
+                          country="BRA"
+                        />
+                      </div>
+                    </Fragment>
+                  ) : (
+                    <div className="pv4">
+                      <AvailabilitySubscriber skuId={this.selectedItem.itemId} />
+                    </div>
+                  )}
+                  <div className="flex w-100 pv2">
+                    <div className="pv2 pr3 f6">
+                      <FormattedMessage id="share.label" />:
+                    </div>
+                    <Share
+                      {...this.props.share}
+                      loading={!path(['name'], this.selectedItem)}
+                      title={intl.formatMessage(
+                        { id: 'share.title' },
+                        {
+                          product: path(['productName'], product),
+                          sku: path(['name'], this.selectedItem),
+                          store: account,
+                        }
+                      )}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="pv4 w-100">
-              <hr className="b--black-10" size="0" />
-            </div>
-            <div className="vtex-product-details__description-container pv2 w-100 h-100">
-              <ProductDescription
-                specifications={path(['properties'], product)}
-                skuName={path(['name'], this.selectedItem)}
-                description={path(['description'], product)}
-              />
+              <div className="pv4 w-100">
+                <hr className="b--black-10" size="0" />
+              </div>
+              <div className="vtex-product-details__description-container pv2 w-100 h-100">
+                <ProductDescription
+                  specifications={path(['properties'], product)}
+                  skuName={path(['name'], this.selectedItem)}
+                  description={path(['description'], product)}
+                />
+              </div>
             </div>
           </div>
         )}
