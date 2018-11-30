6dd339e58f827bcf10ac8e5f0e7f922b2303fcb8
SamuraiExx <samuraiexx@gmail.com>
2018-11-30 09:40:35 -0200

    Add FixedButton component

---
+++ react/components/FixedButton.js
@@ -0,0 +1,53 @@
+import React, { Component, Fragment } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class FixedButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ref = React.createRef()
+    this.state = { mounted: false }
+    this.el = document.createElement('div')
+  }
+
+  componentDidMount() {
+    this.fixedRoot = this.ref.current
+
+    while(this.fixedRoot &&
+          window.getComputedStyle(this.fixedRoot).
+          getPropertyValue('overflow') !== 'auto' )
+      this.fixedRoot = this.fixedRoot.parentElement
+
+    if(!this.fixedRoot) this.fixedRoot = document.body
+
+    this.fixedRoot && this.fixedRoot.appendChild(this.el)
+    this.setState({ mounted: true })
+  }
+
+  componentWillUnmount() {
+    this.fixedRoot && this.fixedRoot.removeChild(this.el)
+  }
+
+  render() {
+    const { mounted } = this.state
+
+    if(!mounted) return <div ref={this.ref}/>
+
+    const children = this.props.children
+
+    return ReactDOM.createPortal(
+      <Fragment>
+        <div className="o-0">
+          {children}
+        </div>
+        <div className="fixed bottom-0 w-100 z-999">
+          {children}
+        </div>
+      </Fragment>,
+      this.el,
+    )
+  }
+}
+
+export default FixedButton
