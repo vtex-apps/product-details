4e9eeb3b65d5f447d360acae562eddbaef98dd5e
SamuraiExx <samuraiexx@gmail.com>
2018-11-30 12:03:04 -0200

    Update page layout

--- react/ProductDetails.js
+++ react/ProductDetails.js
@@ -226,107 +226,107 @@ class ProductDetails extends Component {
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="vtex-product-details__details-container w-50-ns w-100-s pl5-ns">
-                <div className="fl-ns w-100">
-                  <div className="vtex-product-details__name-container pv2">
-                    <ProductName
-                      styles={productNameLoaderStyles}
-                      name={path(['productName'], product)}
-                      skuName={path(['name'], this.selectedItem)}
-                      brandName={path(['brand'], product)}
-                      productReference={path(['productReference'], product)}
-                      {...this.props.name}
-                    />
-                  </div>
-                  {(Number.isNaN(
-                    +path(['AvailableQuantity'], this.commertialOffer)
-                    ) ||
-                    path(['AvailableQuantity'], this.commertialOffer) > 0) && (
-                    <div className="vtex-product-details__price-container pt1">
-                      <ProductPrice
-                        styles={productPriceLoaderStyles}
-                        listPriceContainerClass="t-small-s t-small-ns c-muted-2"
-                        sellingPriceLabelClass="t-heading-6-s t-heading-5-ns dib"
-                        listPriceLabelClass="dib strike"
-                        listPriceClass="ph2 dib strike"
-                        sellingPriceContainerClass="pv1 b c-muted-1"
-                        sellingPriceClass="t-heading-2-s t-heading-1-ns dib ph2"
-                        installmentClass="t-heading-6-s t-heading-5-ns"
-                        savingsClass="t-mini-s t-small-ns"
-                        installmentContainerClass="t-mini-s t-small-ns c-muted-2"
-                        interestRateClass="dib ph2"
-                        savingsContainerClass="c-muted-2"
-                        savingsClass="dib t-small-ns t-mini-s"
-                        loaderClass="h4-s mw6-s pt2-s"
-                        listPrice={path(['ListPrice'], this.commertialOffer)}
-                        sellingPrice={path(['Price'], this.commertialOffer)}
-                        installments={path(['Installments'], this.commertialOffer)}
-                        large
-                        {...this.props.price}
+                <div className="vtex-product-details__details-container w-40-ns w-100 pl5-ns">
+                  <div className="fl-ns w-100">
+                    <div className="vtex-product-details__name-container pv2">
+                      <ProductName
+                        styles={productNameLoaderStyles}
+                        name={path(['productName'], product)}
+                        skuName={path(['name'], this.selectedItem)}
+                        brandName={path(['brand'], product)}
+                        productReference={path(['productReference'], product)}
+                        {...this.props.name}
                       />
                     </div>
-                  )}
-                  {product && this.selectedItem.variations
-                  && this.selectedItem.variations.length > 0
-                  && (
-                    <Fragment>
-                      <div className="pv2">
-                        <hr className="o-30" size="1" />
-                      </div>
-                      <SKUSelector
-                        skuItems={this.skuItems}
-                        skuSelected={this.selectedItem}
-                        productSlug={product.linkText}
-                      />
-                    </Fragment>
-                  )}
-                  <div className="pv2">
-                    <hr className="o-30" size="1" />
-                  </div>
-                  {showBuyButton ? (
-                    <Fragment>
-                      <div className="pv2 dn db-l">
-                        <PBuyButton {...buyButtonProps}>
-                          <FormattedMessage id="addToCartButton.label"/>
-                        </PBuyButton>
-                        <FixedButton>
-                          <div className="dn-l bg-base w-100 pa3">
-                            <PBuyButton {...buyButtonProps}>
-                              <FormattedMessage id="addToCartButton.label"/>
-                            </PBuyButton>
-                          </div>
-                        </FixedButton>
-                      </div>
-                      <div className="pv4">
-                        <ShippingSimulator
-                          skuId={path(['itemId'], this.selectedItem)}
-                          seller={this.sellerId}
-                          country={country}
+                    {(Number.isNaN(
+                      +path(['AvailableQuantity'], this.commertialOffer)
+                      ) ||
+                      path(['AvailableQuantity'], this.commertialOffer) > 0) && (
+                      <div className="vtex-product-details__price-container pt1">
+                        <ProductPrice
+                          styles={productPriceLoaderStyles}
+                          listPriceContainerClass="t-small-s t-small-ns c-muted-2"
+                          sellingPriceLabelClass="t-heading-6-s t-heading-5-ns dib"
+                          listPriceLabelClass="dib strike"
+                          listPriceClass="ph2 dib strike"
+                          sellingPriceContainerClass="pv1 b c-muted-1"
+                          sellingPriceClass="t-heading-2-s t-heading-1-ns dib ph2"
+                          installmentClass="t-heading-6-s t-heading-5-ns"
+                          savingsClass="t-mini-s t-small-ns"
+                          installmentContainerClass="t-mini-s t-small-ns c-muted-2"
+                          interestRateClass="dib ph2"
+                          savingsContainerClass="c-muted-2"
+                          savingsClass="dib t-small-ns t-mini-s"
+                          loaderClass="h4-s mw6-s pt2-s"
+                          listPrice={path(['ListPrice'], this.commertialOffer)}
+                          sellingPrice={path(['Price'], this.commertialOffer)}
+                          installments={path(['Installments'], this.commertialOffer)}
+                          large
+                          {...this.props.price}
                         />
                       </div>
-                    </Fragment>
-                  ) : (
-                    <div className="pv4">
-                      <AvailabilitySubscriber skuId={this.selectedItem.itemId} />
+                    )}
+                    {product && this.selectedItem.variations
+                    && this.selectedItem.variations.length > 0
+                    && (
+                      <Fragment>
+                        <div className="pv2">
+                          <hr className="o-30" size="1" />
+                        </div>
+                        <SKUSelector
+                          skuItems={this.skuItems}
+                          skuSelected={this.selectedItem}
+                          productSlug={product.linkText}
+                        />
+                      </Fragment>
+                    )}
+                    <div className="pv2">
+                      <hr className="o-30" size="1" />
                     </div>
-                  )}
-                  <div className="flex w-100 pv2">
-                    <div className="pv2 pr3 f6">
-                      <FormattedMessage id="share.label" />:
+                    {showBuyButton ? (
+                      <Fragment>
+                        <div className="pv2 dn db-l">
+                          <PBuyButton {...buyButtonProps}>
+                            <FormattedMessage id="addToCartButton.label"/>
+                          </PBuyButton>
+                          <FixedButton>
+                            <div className="dn-l bg-base w-100 pa3">
+                              <PBuyButton {...buyButtonProps}>
+                                <FormattedMessage id="addToCartButton.label"/>
+                              </PBuyButton>
+                            </div>
+                          </FixedButton>
+                        </div>
+                        <div className="pv4">
+                          <ShippingSimulator
+                            skuId={path(['itemId'], this.selectedItem)}
+                            seller={this.sellerId}
+                            country={country}
+                          />
+                        </div>
+                      </Fragment>
+                    ) : (
+                      <div className="pv4">
+                        <AvailabilitySubscriber skuId={this.selectedItem.itemId} />
+                      </div>
+                    )}
+                    <div className="flex w-100 pv2">
+                      <div className="pv2 pr3 f6">
+                        <FormattedMessage id="share.label" />:
+                      </div>
+                      <Share
+                        {...this.props.share}
+                        loading={!path(['name'], this.selectedItem)}
+                        title={intl.formatMessage(
+                          { id: 'share.title' },
+                          {
+                            product: path(['productName'], product),
+                            sku: path(['name'], this.selectedItem),
+                            store: account,
+                          }
+                        )}
+                      />
                     </div>
-                    <Share
-                      {...this.props.share}
-                      loading={!path(['name'], this.selectedItem)}
-                      title={intl.formatMessage(
-                        { id: 'share.title' },
-                        {
-                          product: path(['productName'], product),
-                          sku: path(['name'], this.selectedItem),
-                          store: account,
-                        }
-                      )}
-                    />
                   </div>
                 </div>
               </div>
