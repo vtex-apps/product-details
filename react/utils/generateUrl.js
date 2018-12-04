606c2612651e7804caa0e646f5e18e8afa5deae9
SamuraiExx <samuraiexx@gmail.com>
2018-12-04 11:23:30 -0200

    Removed unused function and commented regex

--- react/utils/generateUrl.js
+++ react/utils/generateUrl.js
@@ -2,22 +2,18 @@ export const DEFAULT_WIDTH = 'auto'
 export const DEFAULT_HEIGHT = 'auto'
 export const MAX_WIDTH = 3000
 export const MAX_HEIGHT = 4000
-export const RESIZE_RATE = 1 / 2
 
-const baseUrlRegex = new RegExp(/.+ids\/(\d+)(?:-(\d+)-(\d+)|)\//)
-const sizeRegex = new RegExp(/-(\d+)-(\d+)/)
+/**
+ * Having the url above as base,
+ * https://storecomponents.vteximg.com.br/arquivos/ids/155472/Frame-3.jpg?v=636793763985400000
+ * the following regex will match https://storecomponents.vteximg.com.br/arquivos/ids/155472
+ * @type {RegExp}
+ */
+const baseUrlRegex = new RegExp(/.+ids\/(\d+)/)
 
 export function cleanImageUrl(imageUrl) {
-  let resizedImageUrl = imageUrl
   const result = baseUrlRegex.exec(imageUrl)
-  if (result.length > 0) {
-    if (result.length === 4 && result[2] && result[3]) {
-      resizedImageUrl = result[0].replace(sizeRegex, '')
-    } else {
-      resizedImageUrl = result[0]
-    }
-    return resizedImageUrl
-  }
+  if (result.length > 0) return result[0]
 }
 
 export function changeImageUrlSize(
@@ -30,10 +26,6 @@ export function changeImageUrlSize(
   typeof height === 'number' && (height = Math.min(height, MAX_HEIGHT))
 
   imageUrl = cleanImageUrl(imageUrl)
-  const resizedImageUrl = imageUrl.slice(0, -1) // Remove last "/"
-  return `${resizedImageUrl}-${width}-${height}`
-}
 
-export const nextImageUrl = (imageUrl, initialWidth) => (tryCount) => {
-  return changeImageUrlSize(imageUrl, Math.floor(initialWidth * Math.pow(RESIZE_RATE, tryCount)))
+  return `${imageUrl}-${width}-${height}`
 }
