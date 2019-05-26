0956a39bf4c1c990cb8c04e5f0d1b0a7f5b0de74
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-26 19:57:24 -0300

    Check if productQuery is not nil before getting product out of it

--- react/ProductDetails.js
+++ react/ProductDetails.js
@@ -245,7 +245,7 @@ class ProductDetails extends Component {
 
   render() {
     const {
-      productQuery: { product },
+      productQuery,
       categories,
       categoryTree,
       runtime: {
@@ -258,6 +258,13 @@ class ProductDetails extends Component {
       showHighlight,
       thumbnailPosition,
     } = this.props
+
+    const product = productQuery && productQuery.product
+    
+    if (!product) {
+      return null
+    }
+
     const { selectedQuantity } = this.state
 
     const showBuyButton =
