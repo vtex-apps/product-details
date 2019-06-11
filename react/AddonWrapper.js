3320f229e557e8f2c9eb0da898d482e22a8e9404
ThaynanAndrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-06-11 16:04:55 -0300

    Fix heart icon position.

--- react/AddonWrapper.js
+++ react/AddonWrapper.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import { ExtensionPoint, useRuntime } from 'vtex.render-runtime'
 import { ProductContext } from 'vtex.product-context'
 import { path } from 'ramda'
+import classNames from 'classnames'
 
 const AddonWrapper = () => {
     const {
         hints: { mobile },
     } = useRuntime()
-    
     const valuesFromContext = useContext(ProductContext)
     
     const product= {
@@ -16,7 +16,20 @@ const AddonWrapper = () => {
         productId: path(['product', 'productId'], valuesFromContext)
     }
     
-    return <ExtensionPoint id="addon-details-btn" large={!mobile} product={product} />
+    const isMoreThanOneImage = () => {
+        const images =  path(['selectedItem', 'images'], valuesFromContext)
+        return images && images.length > 1
+    }
+
+    const addonDetailsClasses = classNames('', {
+        'ml-20-ns': isMoreThanOneImage()
+    })
+
+    return (
+        <div className={addonDetailsClasses}>
+            <ExtensionPoint id="addon-details-btn" large={!mobile} product={product} />
+        </div>
+    )
 }
 
 export default AddonWrapper
